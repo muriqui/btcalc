383a330959811214673165adf6afb24a66e11498
Dan Lobelle <581050+muriqui@users.noreply.github.com>
2022-09-11 20:16:58 -0400

    Add total and summary functions.

--- src/utils/modifiers.test.ts
+++ src/utils/modifiers.test.ts
@@ -1,7 +1,24 @@
-import { formatModifier } from "./modifiers";
+import {
+  formatModifier,
+  totalModifiers,
+  summarizeModifiers,
+} from "./modifiers";
 
 it("formats modifiers", () => {
   expect(formatModifier(2)).toEqual("+2");
   expect(formatModifier(0)).toEqual("0");
   expect(formatModifier(-2)).toEqual("-2");
+});
+
+const modifiers = new Map<string, number>([
+  ["Walked", 1],
+  ["Ran", 2],
+]);
+
+it("totals modifiers", () => {
+  expect(totalModifiers(modifiers)).toEqual(3);
+});
+
+it("summarizes modifiers", () => {
+  expect(summarizeModifiers(modifiers)).toEqual("Walked +1, Ran +2");
 });
--- src/utils/modifiers.ts
+++ src/utils/modifiers.ts
@@ -8,3 +8,25 @@
 export function formatModifier(value: number): string {
   return (value <= 0 ? "" : "+") + value;
 }
+
+/**
+ * Calculates the total of a list of modifiers.
+ */
+export function totalModifiers(modifiers: Map<string, number>): number {
+  let total = 0;
+  modifiers.forEach((value) => (total += value));
+  return total;
+}
+
+/**
+ * Summarizes a list of modifiers.
+ */
+export function summarizeModifiers(modifiers: Map<string, number>): string {
+  const summary: string[] = [];
+  modifiers.forEach((value, label) => {
+    if (value !== 0) {
+      summary.push(`${label} ${formatModifier(value)}`);
+    }
+  });
+  return summary.join(", ");
+}
