7667e7ca686060b174715b480e90f8288c60a1a9
Dan Lobelle <581050+muriqui@users.noreply.github.com>
2024-10-13 19:15:09 -0400

    Allow Heading to display as though at a different level than the hierarchy requires.

--- src/components/atoms/Heading.tsx
+++ src/components/atoms/Heading.tsx
@@ -1,8 +1,12 @@
 import { HTMLAttributes } from "react";
 
+type HeadingLevels = 1 | 2 | 3 | 4 | 5 | 6;
+
 export interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
-  /** The heading level, from 1 to 6. */
-  level: 1 | 2 | 3 | 4 | 5 | 6;
+  /** The hierarchical heading level, from 1 to 6. */
+  level: HeadingLevels;
+  /** Optionally, display the heading as though it were at a different level than its hierarchical level. */
+  displayLevel?: HeadingLevels;
 }
 
 /**
@@ -10,51 +14,78 @@ export interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
  */
 export default function Heading({
   level,
+  displayLevel,
   className,
   children,
   ...props
 }: HeadingProps) {
-  className = `font-bold tracking-tight text-balance text-gray-900 dark:text-gray-200${className ? " " + className : ""}`;
+  let levelClasses;
+  switch (displayLevel ?? level) {
+    case 1:
+      levelClasses = "text-4xl sm:text-6xl";
+      break;
+
+    case 2:
+      levelClasses = "text-2xl";
+      break;
+
+    case 3:
+      levelClasses = "text-xl";
+      break;
+
+    case 4:
+      levelClasses = "text-lg";
+      break;
+
+    case 5:
+      levelClasses = "text-base";
+      break;
+
+    case 6:
+      levelClasses = "text-sm";
+      break;
+  }
+  className = `${levelClasses} font-bold tracking-tight text-balance text-gray-900 dark:text-gray-200${className ? " " + className : ""}`;
 
   switch (level) {
     case 1:
       return (
-        <h1 className={`text-4xl sm:text-6xl ${className}`} {...props}>
+        <h1 className={className} {...props}>
           {children}
         </h1>
       );
 
     case 2:
       return (
-        <h2 className={`text-2xl ${className}`} {...props}>
+        <h2 className={className} {...props}>
           {children}
         </h2>
       );
 
     case 3:
       return (
-        <h3 className={`text-xl ${className}`} {...props}>
+        <h3 className={className} {...props}>
           {children}
         </h3>
       );
 
     case 4:
       return (
-        <h4 className={`text-lg ${className}`} {...props}>
+        <h4 className={className} {...props}>
           {children}
         </h4>
       );
 
     case 5:
       return (
-        <h5 className={`text-base ${className}`} {...props}>
+        <h5 className={className} {...props}>
           {children}
         </h5>
       );
 
     case 6:
       return (
-        <h6 className={`text-sm ${className}`} {...props}>
+        <h6 className={className} {...props}>
           {children}
         </h6>
       );
