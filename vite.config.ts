ba7826b76b649f7c619454ea282557e14a3c6f54
Dan Lobelle <581050+muriqui@users.noreply.github.com>
2025-04-06 01:40:46 -0400

    Add React Router DevTools.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouterDevTools } from "react-router-devtools";
 
 const isStorybook = process.argv[1]?.includes("storybook");
 
 export default defineConfig({
-  plugins: [!isStorybook && reactRouter(), tsconfigPaths()],
+  plugins: [
+    !isStorybook && reactRouterDevTools(),
+    !isStorybook && reactRouter(),
+    tsconfigPaths(),
+  ],
 });
