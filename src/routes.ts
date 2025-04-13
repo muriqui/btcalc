5cce7c828394ffa569d6ea3605820d8ab3c07d42
Dan Lobelle <581050+muriqui@users.noreply.github.com>
2025-04-12 20:54:02 -0400

    Simplify play route definition.

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,11 @@
-import {
-  type RouteConfig,
-  index,
-  layout,
-  prefix,
-  route,
-} from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 import { Step } from "./types";
 
 export default [
   index("./routes/Home.tsx"),
-  ...prefix("play", [
-    layout("./routes/play/Play.tsx", [
-      index("./routes/play/Setup.tsx"),
-      route(Step.Movement, "./routes/play/movement/Movement.tsx"),
-      route(Step.SelectTargets, "./routes/play/targets/SelectTargets.tsx"),
-    ]),
+  route("play", "./routes/play/Play.tsx", [
+    index("./routes/play/Setup.tsx"),
+    route(Step.Movement, "./routes/play/movement/Movement.tsx"),
+    route(Step.SelectTargets, "./routes/play/targets/SelectTargets.tsx"),
   ]),
 ] satisfies RouteConfig;
