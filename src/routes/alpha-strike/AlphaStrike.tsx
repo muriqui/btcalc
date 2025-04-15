8d241d1aca499c51758ddb5791843b5f3eda03c4
Dan Lobelle <581050+muriqui@users.noreply.github.com>
2025-04-14 21:12:46 -0400

    Move TopBar to molecules.

--- src/routes/alpha-strike/AlphaStrike.tsx
+++ src/routes/alpha-strike/AlphaStrike.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import TopBar from "../../components/organisms/TopBar";
+import TopBar from "../../components/molecules/TopBar";
 import { System } from "~/types";
 
 /**
